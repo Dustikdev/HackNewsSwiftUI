4767e4f9015860105494eb5e1b859899ca873315
Никита Швец <dustikappple@icloud.com>
2022-12-08 14:00:39 +0100

    Initial Commit

---
+++ HackNewsSwiftUI/HackNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsSwiftUIApp.swift
+//  HackNewsSwiftUI
+//
+//  Created by Никита Швец on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
